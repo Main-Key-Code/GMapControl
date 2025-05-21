cac4fea895013b614d8b319163a24fabdaa3015b
Unknown Coder <72923266+Main-Key-Code@users.noreply.github.com>
2025-05-21 09:39:08 +0900

    종료 시 메시지 폼 띠우기~! 종료 할 때 Map dispose 시키기

--- MarkerTest/MainF.cs
+++ MarkerTest/MainF.cs
@@ -12,6 +12,20 @@ namespace MarkerTest
 
         public MainF()
         {
+            // 
+            this.FormClosing += (s, e) =>
+            {
+                if (map != null)
+                {
+                    // 정리의 시간이 필요 하니까
+                    // 그런데 구지 필요 할까나?
+                    // 판단이 안되네. . . 
+
+                    ShowCloseMessage();
+                    map.App.Dispose();
+                }
+            };
+
             InitializeComponent();
 
             //GMapControl gMapCtrl = new GMapControl();
@@ -28,13 +42,30 @@ namespace MarkerTest
             map.App.OnMapZoomChanged += OnMapZoomChanged;
             map.App.OnPositionChanged += OnPositionChanged;
 
+
             // 위/경도 표시 레이블 추가
             map.App.Controls.Add(dynLabel = map.ShowLatLngLabel());
         }
 
+        private void ShowCloseMessage()
+        {
+            Form closeMessageBoxF = new Form();
+            closeMessageBoxF.Text = "Close Message Box";
+            closeMessageBoxF.Size = new Size(300, 200);
+            closeMessageBoxF.StartPosition = FormStartPosition.CenterParent;
+            closeMessageBoxF.FormBorderStyle = FormBorderStyle.FixedDialog;
+            closeMessageBoxF.MaximizeBox = false;
+            closeMessageBoxF.MinimizeBox = false;
+            closeMessageBoxF.ShowInTaskbar = false;
+            closeMessageBoxF.TopMost = true;
+            closeMessageBoxF.BackColor = Color.White;
+
+            closeMessageBoxF.ShowDialog();
+
+        }
         private void OnMapZoomChanged()
         {
-            map.SetPointSave(map.showLat, map.showLng);
+            map.MovePoint(map.showLat, map.showLng);
             dynLabel.Text = $"{map.showLat}:{map.showLng}";
         }
 
--- MarkerTest/MapControl.cs
+++ MarkerTest/MapControl.cs
@@ -1,6 +1,7 @@
 ﻿using GMap.NET;
 using GMap.NET.MapProviders;
 using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,8 @@ namespace MarkerTest
 {
     internal class MapControl
     {
+        // gmapcontrol 필드 추가
         public GMapControl App;
-
-        public double showLat { set; get; }
-        public double showLng { set; get; }
 
         public MapControl(GMapControl app)
         {
@@ -31,15 +30,37 @@ namespace MarkerTest
             showLatLan();
 
             this.App.Zoom = 13;
+
+            this.App.MouseClick += (s, e) =>
+            {
+                if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    //AddCustomMarker(showLat, showLng, Properties.Resources.)
+                    // 오른쪽 클릭 시 현재 위치로 이동
+                    MovePoint(showLat, showLng);
+                }
+            };
         }
 
+        // 위/경도 표시 필드 추가
+        public double showLat { set; get; }
+        public double showLng { set; get; }
+
+        /// <summary>
+        /// 현재 위/경도 값 세팅
+        /// </summary>
         public void showLatLan()
         {
             showLat = this.App.Position.Lat;
             showLng = this.App.Position.Lng;
         }
 
-        public void SetPointSave(double lat, double lng)
+        /// <summary>
+        /// 위치 이동
+        /// </summary>
+        /// <param name="lat"></param>
+        /// <param name="lng"></param>
+        public void MovePoint(double lat, double lng)
         {
             this.App.Position = new PointLatLng(showLat, showLng);
         }
@@ -60,6 +81,29 @@ namespace MarkerTest
             label.Text = $"{showLat} : {showLng}";
 
             return label;
+        }
+
+        // 마커 오버레이 필드 추가
+        private GMapOverlay markerOverlay;
+
+        /// <summary>
+        /// 사용자 지정 이미지로 마커 추가
+        /// </summary>
+        /// <param name="lat"></param>
+        /// <param name="lng"></param>
+        /// <param name="imagePath"></param>
+        public void AddCustomMarker(double lat, double lng, string imagePath)
+        {
+            // 이미지 로드
+            Bitmap markerImage = new Bitmap(imagePath);
+
+            // 커스텀 마커 생성
+            var marker = new GMarkerGoogle(
+                new PointLatLng(lat, lng),
+                markerImage
+            );
+
+            markerOverlay.Markers.Add(marker);
         }
     }
 }
